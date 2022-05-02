6013f4e2428f85393802e8cf234bc64090053a6e
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-05-02 16:07:28 -0300

    Start desktop version

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Delete all caches
-    if ('caches' in window) {
-        caches.keys().then(keys => {
-            keys.forEach(key => {
-                caches.delete(key);
-            });
-        });
-    }
-    // Delete all cookies
-    document.cookie.split(';').forEach(c => {
-        document.cookie = c.replace(/^ +/, '').replace(/=.*/, '=;expires=' + new Date().toUTCString() + ';path=/');
-    });
+    // // Delete all caches
+    // if ('caches' in window) {
+    //     caches.keys().then(keys => {
+    //         keys.forEach(key => {
+    //             caches.delete(key);
+    //         });
+    //     });
+    // }
+    // // Delete all cookies
+    // document.cookie.split(';').forEach(c => {
+    //     document.cookie = c.replace(/^ +/, '').replace(/=.*/, '=;expires=' + new Date().toUTCString() + ';path=/');
+    // });
 })
