a2d68b1f296f2ad9cfee02e83a286633fb47990d
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-05-03 22:33:53 -0300

    update background

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // // Delete all caches
-    // if ('caches' in window) {
-    //     caches.keys().then(keys => {
-    //         keys.forEach(key => {
-    //             caches.delete(key);
-    //         });
-    //     });
-    // }
-    // // Delete all cookies
-    // document.cookie.split(';').forEach(c => {
-    //     document.cookie = c.replace(/^ +/, '').replace(/=.*/, '=;expires=' + new Date().toUTCString() + ';path=/');
-    // });
+    window.addEventListener('scroll', () => {
+        let scroll = window.pageYOffset;
+        const main = document.querySelector('main');
+        const servicesSection = document.querySelector('#services');
+        const staffSection = document.querySelector('#staff');
+        const aboutSection = document.querySelector('#about');
+        const contactSection = document.querySelector('#contact');
+
+        if (scroll > servicesSection.offsetTop && scroll < staffSection.offsetTop) {
+            main.style.backgroundColor = '#EAE670';
+        } else if (scroll > staffSection.offsetTop  && scroll < aboutSection.offsetTop) {
+            main.style.backgroundColor = '#81ecec';
+        } else if(scroll > aboutSection.offsetTop && scroll < contactSection.offsetTop) {
+            main.style.backgroundColor = '#EE4132';
+        } else if(scroll > contactSection.offsetTop) {
+            main.style.backgroundColor = '#fafafa';
+        } else {
+            main.style.backgroundColor = '#e8e8e8';
+        }
+    });
+
+    // cover navbar on scroll down  and discover navbar on scroll up
+    const navbar = document.querySelector('nav ul');
+    const logo = document.querySelector('header img.header-logo');
+    const navbarHeight = navbar.offsetHeight;
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > navbarHeight) {
+            navbar.classList.add('navbar-cover');
+            logo.classList.add('header-logo-cover');
+        } else {
+            navbar.classList.remove('navbar-cover');
+            logo.classList.remove('header-logo-cover');
+        }
+    });
+    
 })
